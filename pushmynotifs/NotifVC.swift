90400c919025380b54bc05c99d52ac7ba991e922
Shin Park <shinpark@shins-mac-mini.home>
2017-03-26 11:44:36 -0500

    Firebase Push Notifications - Connecting via code to Firebase messaging

---
+++ pushmynotifs/NotifVC.swift
@@ -0,0 +1,22 @@
+//
+//  NotifVC.swift
+//  pushmynotifs
+//
+//  Created by Shin Park on 3/25/17.
+//  Copyright © 2017 shinDev. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseInstanceID
+import FirebaseMessaging
+
+class NotifVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        FIRMessaging.messaging().subscribe(toTopic: "/topics/news")
+    }
+}
+
